590ccde322103787d3ab10baed7f1c421af04eca
maxime-bnzsa <105300909+maxime-bnzsa@users.noreply.github.com>
2022-06-20 14:16:41 +0200

    doc upload

--- dashboard.py
+++ dashboard.py
@@ -1,44 +1,37 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import scipy.stats
+from scipy.stats import norm
+import altair as alt
 
-@st.cache
-def get_UN_data():
-    AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
+st.set_page_config(
+    page_title="EDA No-Code", page_icon="📊", initial_sidebar_state="expanded"
+)
 
-try:
-    df = get_UN_data()
-    countries = st.multiselect(
-        "Choose countries", list(df.index), ["China", "United States of America"]
-    )
-    if not countries:
-        st.error("Please select at least one country.")
-    else:
-        data = df.loc[countries]
-        data /= 1000000.0
-        st.write("### Gross Agricultural Production ($B)", data.sort_index())
-
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
+st.write(
     """
-        % e.reason
-    )
+# 📊 EDA No-Code
+Upload your CSV document for an Exploratory Data Analysis.
+"""
+)
+
+uploaded_file = st.file_uploader("Upload CSV", type=".csv")
+
+ft_default = None
+result_default = None
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+
+    st.markdown("### Data preview")
+    st.dataframe(df.head())
+
+    st.markdown("### Select columns for analysis")
+    with st.form(key="my_form"):
+        ft = st.multiselect(
+            "Data column",
+            options=df.columns,
+            help="Select which column refers to the feature your are exploring.",
+            default=ft_default,
+        )
